4121435575cd603057936d7eb760742534f39e4d
luisggf <luis.guigfonseca@gmail.com>
2023-06-28 02:21:23 -0300

    better treatment

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def data_api_requests():
         except:
             print("Conteúdo indisponível")
 
-
     dados_final = pd.DataFrame(dados)
 
     for index, votacao in dados_final.iterrows():
@@ -74,52 +73,30 @@ def data_api_requests():
             graph.add_two_way_edge(deputado_nome, outro_deputado_nome, 1)
     return graph
 
-
-
-import pandas as pd
-
+# processamento e armazenamento dos arquivos
 def save_files_to_csv(graph):
+    # Salvar os arquivos
+    output_filename_edges = 'concordancia_deput_arestas.csv'
+    output_filename_participation = 'concordancia_deput_participacao.csv'
+    
     # Gerar o conteúdo do arquivo de arestas
     edges_content = []
     for node1, edges in graph.adj_list.items():
         for node2, weight in edges.items():
-            edges_content.append([node1, node2, int(weight/2)])
-
-    # Gerar o conteúdo do arquivo de participação
-    participation_content = []
-    for node, edges in graph.adj_list.items():
-        num_participations = sum(weight for _, weight in edges.items())
-        participation_content.append([node, num_participations])
-
-    # Formatar nomes dos nós substituindo espaços por "_"
-    edges_content = [[node1.replace(' ', '_'), node2.replace(' ', '_'), weight] for node1, node2, weight in edges_content]
-    participation_content = [[node.replace(' ', '_'), num_participations] for node, num_participations in participation_content]
-
-    # Criar DataFrame para as arestas
-    df_edges = pd.DataFrame(edges_content, columns=['deputado1', 'deputado2', 'peso'])
-
-    # Criar DataFrame para a participação
-    df_participation = pd.DataFrame(participation_content, columns=['deputado', 'num_votacoes'])
-
-    # Gerar o conteúdo do arquivo de arestas
-    edges_content = [f"{node1} {node2} {weight}" for node1, node2, weight in edges_content]
-
-    # Gerar o conteúdo do arquivo de participação
-    participation_content = [f"{node} {num_participations}" for node, num_participations in participation_content]
-
-    # Salvar os arquivos
-    output_filename_edges = 'concordancia_deput_arestas.csv'
-    output_filename_participation = 'concordancia_deput_participacao.csv'
+            edges_content.append([node1.replace(' ', '_'), node2.replace(' ', '_'), int(weight/2)])
 
     with open(output_filename_edges, 'w', encoding='utf-8') as file_edges:
-        file_edges.write(f"{graph.node_count} {graph.edge_count}\n")
-        file_edges.write('\n'.join(edges_content))
+        file_edges.write(f"Nós: {graph.node_count} Arestas: {graph.edge_count}\n")
+        for nodes in edges_content:
+            file_edges.write(' '.join(map(str, nodes)) + '\n')
 
     with open(output_filename_participation, 'w', encoding='utf-8') as file_participation:
-        file_participation.write(f"{graph.node_count}\n")
-        file_participation.write('\n'.join(participation_content))
+        file_participation.write(f"Numero de participantes das votações: {graph.node_count}\n")
+        for node, num_participations in graph.adj_list.items():
+            file_participation.write(f"{node.replace(' ', '_')} {len(num_participations)}\n")
 
     print(f"Arquivos '{output_filename_edges}' e '{output_filename_participation}' salvos com sucesso.")
+
 
 graph = Weighted_Graph()
 graph = data_api_requests()
